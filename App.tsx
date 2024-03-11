158aa554d9a976c931eccec6bfd9ec89c3b1dc5a
NoahSmo <noah.petel.app@gmail.com>
2024-03-11 16:26:59 +0100

    🚧WIP Auth

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Navigation from './components/Navigation';
+import Login from "./pages/Login";
 
+const isUserLoggedIn = false;
 export default function App() {
     return (
         <Navigation/>
+
+        // <Login/>
+
+        // {isUserLoggedIn && <Navigation/>}
     );
 }
