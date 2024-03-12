aed3255fc023795da3d899bbeac27c9f7f97347d
NoahSmo <noah.petel.app@gmail.com>
2024-03-12 16:25:12 +0100

    🚧WIP User Modal

--- context/UserContext.js
+++ context/UserContext.js
@@ -4,6 +4,10 @@ import {persist} from "zustand/middleware";
 export const useUserContext = create(
     persist(
         (set) => ({
+            userId: "",
+            setUserId: (userId) => set({ userId }),
+            removeUserId: () => set({ userId: "" }),
+
             userImage: "../assets/Avatars/astro_blue.png",
             setUserImage: (userImage) => set({ userImage }),
         }),
