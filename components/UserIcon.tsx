d6a17e734da668dd2955fad8a0de487ee74e9f7f
NoahSmo <noah.petel.app@gmail.com>
2024-03-13 19:45:33 +0100

    🚧WIP User Icon

--- components/UserIcon.tsx
+++ components/UserIcon.tsx
@@ -1,10 +1,25 @@
-import {View, Text, Image} from "react-native";
+import {Image, Pressable, StyleSheet, View} from "react-native";
 
-export default function UserIcon() {
+// @ts-ignore
+export default function UserIcon({navigation}) {
     return (
-        <View>
-            <Text>UserIcon</Text>
-            <Image source={require('../assets/Logos/logo.png')}/>
+        <View style={styles.container}>
+            <Pressable onPress={() => navigation.navigate('Profile')}>
+                <Image source={require('../assets/Avatars/astro_blue.png')} style={{width: 50, height: 50}}/>
+                <></>
+            </Pressable>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        height: 50,
+        width: 50,
+
+
+        position: 'absolute',
+        top: 50,
+        right: 20,
+    },
+})
